d82877dc23d2d1d86fecb3021e976bf992e856fb
Salar Nosrati-Ershad <snosratiershad@gmail.com>
2025-01-29 12:04:52 +0330

    draft: rewrite all commands_lookup queries in sqlc

--- internal/db/query.sql
+++ internal/db/query.sql
@@ -1,92 +1,167 @@
 -- name: GetUserEmailsWithFingerprint :many
-SELECT email FROM ssh_keys
-WHERE fingerprint = ?;
+SELECT
+    email
+FROM
+    ssh_keys
+WHERE
+    fingerprint = ?;
 
 -- name: GetKeyInfosWithEmail :many
-SELECT fingerprint, created_at FROM ssh_keys
-WHERE email = ?
-ORDER BY created_at ASC;
+SELECT
+    fingerprint,
+    created_at
+FROM
+    ssh_keys
+WHERE
+    email = ?
+ORDER BY
+    created_at ASC;
 
 -- name: GetAllowedUsersWithGranterEmail :many
-SELECT grantee_email, created_at FROM email_permissions
-WHERE granter_email = ?
-ORDER BY created_at ASC;
+SELECT
+    grantee_email,
+    created_at
+FROM
+    email_permissions
+WHERE
+    granter_email = ?
+ORDER BY
+    created_at ASC;
 
 -- name: CountFingerprintWithEmailAndFingerPrint :one
-SELECT COUNT(fingerprint) FROM ssh_keys
-WHERE email = ? AND fingerprint = ?;
+SELECT
+    COUNT(fingerprint)
+FROM
+    ssh_keys
+WHERE
+    email = ?
+    AND fingerprint = ?;
 
 -- name: GetVerificationCodeEmailsWithFingerprintAndCode :many
-SELECT email FROM verification_codes
-WHERE fingerprint = ? AND code = ?;
+SELECT
+    email
+FROM
+    verification_codes
+WHERE
+    fingerprint = ?
+    AND code = ?;
 
 -- name: CountFingerprintWithEmail :one
-SELECT COUNT(fingerprint) FROM ssh_keys
-WHERE email = ?;
+SELECT
+    COUNT(fingerprint)
+FROM
+    ssh_keys
+WHERE
+    email = ?;
 
 -- name: AddVerificationCode :one
-INSERT INTO verification_codes (email, fingerprint, code)
-VALUES (?, ?, ?)
-RETURNING *;
+INSERT INTO
+    verification_codes (email, fingerprint, code)
+VALUES
+    (?, ?, ?) RETURNING *;
 
 -- name: DeleteVerificationCodeWithFingerprintAndCode :exec
 DELETE FROM verification_codes
-WHERE fingerprint = ? and code = ?;
+WHERE
+    fingerprint = ?
+    and code = ?;
 
 -- name: AddSSHKey :one
-INSERT INTO ssh_keys (fingerprint, email)
-VALUES (?, ?)
-RETURNING *;
+INSERT INTO
+    ssh_keys (fingerprint, email)
+VALUES
+    (?, ?) RETURNING *;
 
 -- name: DeletePermissionsWithGranterEmail :exec
 DELETE FROM email_permissions
-WHERE granter_email = ?;
+WHERE
+    granter_email = ?;
 
 -- name: DeletePermissionsWithGranteeEmail :exec
 DELETE FROM email_permissions
-WHERE grantee_email = ?;
+WHERE
+    grantee_email = ?;
 
 -- name: DeleteVerificationCodesWithFingerprint :exec
 DELETE FROM verification_codes
-WHERE fingerprint = ?;
+WHERE
+    fingerprint = ?;
 
 -- name: DeleteAdminFingerprintsWithFingerprint :exec
 DELETE FROM admin_fingerprints
-WHERE fingerprint = ?;
+WHERE
+    fingerprint = ?;
 
 -- name: DeleteSSHKeysWithFingerprint :exec
 DELETE FROM ssh_keys
-WHERE fingerprint = ?;
+WHERE
+    fingerprint = ?;
 
 -- name: CountAdminFingerprintswithFingerprint :one
-SELECT COUNT(fingerprint) FROM admin_fingerprints
-WHERE fingerprint = ?;
+SELECT
+    COUNT(fingerprint)
+FROM
+    admin_fingerprints
+WHERE
+    fingerprint = ?;
 
 -- name: CountAdminFingerprints :one
-SELECT COUNT(fingerprint) FROM admin_fingerprints;
+SELECT
+    COUNT(fingerprint)
+FROM
+    admin_fingerprints;
 
 -- name: GetAdminFingerprints :many
-SELECT fingerprint, created_at FROM admin_fingerprints
-ORDER BY created_at ASC;
+SELECT
+    fingerprint,
+    created_at
+FROM
+    admin_fingerprints
+ORDER BY
+    created_at ASC;
 
 -- name: AddAdminFingerprint :exec
-INSERT INTO admin_fingerprints (fingerprint)
-VALUES (?)
-RETURNING *;
+INSERT INTO
+    admin_fingerprints (fingerprint)
+VALUES
+    (?) RETURNING *;
 
 -- name: DeleteAdminFingerprintWithFingerprint :exec
 DELETE FROM admin_fingerprints
-WHERE fingerprint = ?;
+WHERE
+    fingerprint = ?;
 
 -- name: CountEmailPermissionsWithGranterAndGranteeEmail :one
-SELECT COUNT(granter_email) FROM email_permissions
-WHERE granter_email = ? AND grantee_email = ?;
+SELECT
+    COUNT(granter_email)
+FROM
+    email_permissions
+WHERE
+    granter_email = ?
+    AND grantee_email = ?;
 
 -- name: AddEmailPermission :exec
-INSERT INTO email_permissions (granter_email, grantee_email)
-VALUES (?, ?)
-RETURNING *;
+INSERT INTO
+    email_permissions (granter_email, grantee_email)
+VALUES
+    (?, ?) RETURNING *;
 
 -- name: DeleteEmailPermissionsWithGranterAndGranteeEmail :exec
 DELETE FROM email_permissions
-WHERE granter_email = ? AND grantee_email = ?;
+WHERE
+    granter_email = ?
+    AND grantee_email = ?;
+
+-- name: GetTargetInfoWithFingerprintAndGranterEmailAndGranteeEmail :many
+SELECT
+    ssh_keys.email
+FROM
+    ssh_keys
+    LEFT JOIN email_permissions ON email_permissions.granter_email = ssh_keys.email
+    AND email_permissions.grantee_email = ?
+WHERE
+    ssh_keys.fingerprint = ?
+    AND (
+        ssh_keys.email = ?
+        OR email_permissions.grantee_email IS NOT NULL
+    );
