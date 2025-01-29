87caf6d374e05a70a344f9755c1c4c310ed52469
Salar Nosrati-Ershad <snosratiershad@gmail.com>
2025-01-29 18:37:03 +0330

    add go generate command and correct sqlc configuration

--- internal/db/gen.go
+++ internal/db/gen.go
@@ -2,4 +2,4 @@ package db
 
 //go:generate sh -c "rm ./keysdb.sqlite3; exit 0"
 //go:generate sqlite3 -init ./schema.sql ./keysdb.sqlite3
-//go:generate go run github.com/go-jet/jet/v2/cmd/jet -source=sqlite -schema=main -path=$PWD/.gen -dsn=$PWD/keysdb.sqlite3
+//go:generate sqlc generate
